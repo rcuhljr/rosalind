6ac38d13b6e0e2bdb03365a505056585fb74e0c0
Robert C. Uhl <rcuhl@sep.com>
2014-04-18 14:16:44 -0400

    started outputting to files after copy paste in console error caused a failed test. /sigh

--- rosalind_2.rb
+++ rosalind_2.rb
@@ -1,6 +1,7 @@
 load 'dna.rb'
-
-File.foreach("data_2.txt") do |line|
-	dna = DNA.new(line)
-	puts "#{dna.transcribe}"
+File.open('results.txt', "w") do |output|
+	File.foreach("data_2.txt") do |line|
+		dna = DNA.new(line)
+		output.puts "#{dna.transcribe}"
+	end
 end
